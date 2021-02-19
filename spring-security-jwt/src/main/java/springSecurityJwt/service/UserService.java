585218ac0ab8c321de07b615470a881dfcf41697
arjun <arjunyadav102030@gmail.com>
2021-02-19 10:35:32 +0530

    first commit

--- spring-security-jwt/src/main/java/springSecurityJwt/service/UserService.java
+++ spring-security-jwt/src/main/java/springSecurityJwt/service/UserService.java
@@ -13,7 +13,7 @@ public class UserService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		return new User("admin","password",new ArrayList<>());
+		return new User("username","password",new ArrayList<>());
 	}
 
 }
