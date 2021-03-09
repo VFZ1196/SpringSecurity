3db1e66c36438043069e3017083380b9096edec5
arjun <arjunyadav102030@gmail.com>
2021-03-09 15:47:48 +0530

    first commit

--- spring-security-jwt/src/main/java/springSecurityJwt/controller/HomeController.java
+++ spring-security-jwt/src/main/java/springSecurityJwt/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import springSecurityJwt.service.UserService;
 import springSecurityJwt.utility.JWTUtility;
 
 @RestController
+@CrossOrigin
 public class HomeController {
 	
 	@Autowired
@@ -27,7 +29,7 @@ public class HomeController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String home() {
 		return "Hello World";
 	}
